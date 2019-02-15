4236b0b69e9189abb9877c4ca6dcf8b6f6c2c972
Afoninsky <vkfont@gmail.com>
2019-02-15 14:16:07 +0300

    chore: WIP

--- src/actions/echo.js
+++ src/actions/echo.js
@@ -1,10 +1,19 @@
 const { ActionTransport } = require('@microfleet/core')
 
 async function handler() {
-  console.log(...arguments)
+  console.log(123)
+  // console.log(...arguments)
+  return {
+    qwe: 'asd'
+  }
 }
 
-handler.transports = [ActionTransport]
+handler.transports = [ActionTransport.http]
+handler.transportOptions = {
+  [ActionTransport.http]: {
+    methods: ['get']
+  }
+}
 handler.schema = 'echo'
 
 module.exports = handler

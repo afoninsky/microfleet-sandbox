a5d5da73990c0aab79d4c26b5334995b9382c013
Afoninsky <vkfont@gmail.com>
2019-02-15 19:22:29 +0300

    chore: WIP

--- src/actions/echo.js
+++ src/actions/echo.js
@@ -1,11 +1,39 @@
 const { ActionTransport } = require('@microfleet/core')
 
+const logHeaders = ['x-request-id', 'x-b3-traceid']
+
+const proxyHeaders = [
+  'x-request-id',
+  'x-b3-traceid',
+  'x-b3-spanid',
+  'x-b3-parentspanid',
+  'x-b3-sampled',
+  'x-b3-flags',
+  'x-ot-span-context'
+]
+
 async function handler(request) {
-  const { headers } = request
-  // request.transportRequest - hapi request goes here
-  console.log('incoming request, headers:')
-  console.log(headers)
-  return headers
+  const { headers, transportRequest } = request
+  const response = transportRequest.generateResponse('hello world')
+
+  // proxy headers request -> response
+  proxyHeaders.forEach(headerName => {
+    if (headerName in headers) {
+      response.header(headerName, headers[headerName])
+    }
+  })
+
+  // log something including debug ids
+  const traces = {}
+  logHeaders.forEach(headerName => {
+    if (headerName in headers) {
+      traces[headerName] = headers[headerName]
+    }
+  })
+  const log = this.log.child(traces)
+  log.info('hello world')
+
+  return response
 }
 
 handler.transports = [ActionTransport.http]
